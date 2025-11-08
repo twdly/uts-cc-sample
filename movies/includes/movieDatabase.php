01c44773ad4914ae03dee46bc9ad3aea7687513c
Tai <taiwoodley@gmail.com>
2025-11-09 10:48:53 +1100

    Add debug code

--- movies/includes/movieDatabase.php
+++ movies/includes/movieDatabase.php
@@ -4,10 +4,19 @@ $username = getenv('RDS_USERNAME');
 $password = getenv('RDS_PASSWORD');
 $dbname = "moviedb";
 
-$conn = mysqli_connect($servername, $username, $password,$dbname );
+echo "RDS_HOSTNAME: " . getenv('RDS_HOSTNAME') . "\n";
+echo "RDS_USERNAME: " . getenv('RDS_USERNAME') . "\n";
+echo "RDS_PASSWORD: " . (getenv('RDS_PASSWORD') ? 'SET' : 'NOT SET') . "\n";
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+?>
+
 
 ?>
