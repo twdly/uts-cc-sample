051e3b308dcbd233e8f57cc6e9e31030f166d881
Tai <taiwoodley@gmail.com>
2025-11-09 11:09:43 +1100

    Maybe this time

--- movies/includes/movieDatabase.php
+++ movies/includes/movieDatabase.php
@@ -4,9 +4,9 @@ $username = $_SERVER['RDS_USERNAME'];
 $password = $_SERVER['RDS_PASSWORD'];
 $dbname = "moviedb";
 
-echo "RDS_HOSTNAME: " . $_SERVER('RDS_HOSTNAME') . "\n";
-echo "RDS_USERNAME: " . $_SERVER('RDS_USERNAME') . "\n";
-echo "RDS_PASSWORD: " . ($_SERVER('RDS_PASSWORD') ? 'SET' : 'NOT SET') . "\n";
+echo "RDS_HOSTNAME: " . $_SERVER['RDS_HOSTNAME'] . "\n";
+echo "RDS_USERNAME: " . $_SERVER['RDS_USERNAME'] . "\n";
+echo "RDS_PASSWORD: " . ($_SERVER['RDS_PASSWORD'] ? 'SET' : 'NOT SET') . "\n";
 
 $conn = mysqli_connect($servername, $username, $password,$dbname );
 // Check connection
