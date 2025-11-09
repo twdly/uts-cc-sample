bfecdc0a894603038c1963dfaafe32cdc5da6389
Tai <taiwoodley@gmail.com>
2025-11-09 11:03:06 +1100

    Add env echoes back

--- books/includes/bookDatabase.php
+++ books/includes/bookDatabase.php
@@ -4,6 +4,10 @@ $username = $_SERVER('RDS_USERNAME');
 $password = $_SERVER('RDS_PASSWORD');
 $dbname = "bookstore";
 
+echo "RDS_HOSTNAME: " . $_SERVER('RDS_HOSTNAME') . "\n";
+echo "RDS_USERNAME: " . $_SERVER('RDS_USERNAME') . "\n";
+echo "RDS_PASSWORD: " . ($_SERVER('RDS_PASSWORD') ? 'SET' : 'NOT SET') . "\n";
+
 $bookconn = mysqli_connect($servername, $username, $password, $dbname );
 // Check connection
 if (!$bookconn) {
